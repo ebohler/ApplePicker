86ad9d2ed500f080c7e3dca3bd8e3ff0b7a8f0b4
Evan Bohler <57956682+ebohler@users.noreply.github.com>
2024-09-19 23:33:39 -0500

    basic game logic

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float changeDirChance = 0.1f;
+    public float appleDropDelay = 1f;
+
+
+    void Start()
+    {
+        // Start dropping apples
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropDelay);
+    }
+
+    void Update()
+    {
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Random.value < changeDirChance)
+        {
+            
+            speed *= -1; // Change direction
+        }
+    }
+}
